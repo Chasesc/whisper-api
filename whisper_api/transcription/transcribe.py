aa2055a3581c1d0405e728f753f0e7e050848ecc
Chasesc <schelthoff.chase@gmail.com>
2024-03-10 09:01:38 -0400

    use best device by default

--- whisper_api/transcription/transcribe.py
+++ whisper_api/transcription/transcribe.py
@@ -4,24 +4,36 @@ from functools import cache
 from typing import TYPE_CHECKING, Any, Final
 
 import whisper
+import torch
 
 from whisper_api.transcription.schemas import TranscriptionResult
 from whisper_api.transcription.utils import ensure_filepath
 
 if TYPE_CHECKING:
     from numpy import floating, ndarray
-    from torch import device
 
 DEFAULT_MODEL: Final[str] = "base"
 
+def _find_best_device() -> torch.device | None:
+    if torch.cuda.is_available():
+        return torch.device("cuda")
+    # https://github.com/openai/whisper/pull/382 - MPS is not fully supported by pytorch yet.
+    # elif torch.backends.mps.is_available() and torch.backends.mps.is_built():
+    #     return torch.device("mps")
+
+    return None
+
+
+DEFAULT_DEVICE: torch.device | None = _find_best_device()
+
 
 @cache
-def _load_model(model_size: str, device: device | str | None) -> whisper.Whisper:
+def _load_model(model_size: str, device: torch.device | str | None = DEFAULT_DEVICE) -> whisper.Whisper:
     return whisper.load_model(model_size, device=device)
 
 
 def preload_model(
-    model_size: str = DEFAULT_MODEL, device: device | str | None = None
+    model_size: str = DEFAULT_MODEL, device: torch.device | str | None = DEFAULT_DEVICE
 ) -> None:
     # Fill the cache so this work doesn't need to happen during the API call. Not required.
     _load_model(model_size, device)
@@ -36,7 +48,7 @@ def transcribe(
     audio_buffer: ndarray[floating[Any]],
     *,
     model_size: str = DEFAULT_MODEL,
-    device: device | str | None = None,
+    device: torch.device | str | None = DEFAULT_DEVICE,
     word_timestamps: bool = False,
 ) -> TranscriptionResult:
     model = _load_model(model_size, device)
